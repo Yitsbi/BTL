a4680e98da45c49c2796aad967b12f4e4a2c4d05
Yitsbi <lehoangbaobao02@gmail.com>
2024-08-17 07:32:36 +0700

    add some animations to timeline

--- JS/gioithieu.js
+++ JS/gioithieu.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.addEventListener("load", function () {
   let img = document.getElementById("ads-image");
   let inputs = document.querySelectorAll("input[type=radio]");
 
@@ -53,4 +53,42 @@ window.onload = function () {
   };
 
   //MODAL
-};
+
+  //TIMELINE
+  let timeline_contents = document.querySelectorAll(".timeline-content");
+
+  for (let content of timeline_contents) {
+    content.onclick = function () {
+      let text = this.querySelector(".timeline-text");
+      text.classList.toggle("show");
+    };
+  }
+
+  let timeLine = document.querySelectorAll(".timeline > ul > li");
+  for (let t of timeLine) {
+    t.classList.add("wow");
+  }
+
+  let oddTimeline = document.querySelectorAll(
+    ".timeline > ul > li:nth-child(odd)"
+  );
+  for (let t of oddTimeline) {
+    t.classList.add("animate__fadeIn");
+  }
+
+  let evenTimeline = document.querySelectorAll(
+    ".timeline > ul > li:nth-child(even)"
+  );
+  for (let t of evenTimeline) {
+    t.classList.add("animate__fadeIn");
+  }
+
+  wow = new WOW({
+    boxClass: "wow", // default
+    animateClass: "animate__animated", // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true, // default
+  });
+  wow.init();
+});
